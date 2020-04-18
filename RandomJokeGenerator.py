e34a77d0999b0c3f40c5991f7d8fcb6b6df76be6
Appaji Chintimi <52322531+CITIZENDOT@users.noreply.github.com>
2020-04-18 12:52:12 +0530

    Update RandomJokeGenerator.py

--- RandomJokeGenerator.py
+++ RandomJokeGenerator.py
@@ -26,6 +26,7 @@ if error:
         print("4. Dark")
         print("Choose index of the category: ")
         n = int(input())
+url+=dic[n]
 string =""
 print("(Optional) Do you want to blacklist any category or type?, Below are the types")
 print("1. Not Safe for Work")
@@ -38,18 +39,28 @@ print("If you want to ignore, choose -1")
 black = {1: "nsfw", 2: "religious", 3: "political", 4: "racist", 5: "sexist"}
 choice = int(input())
 if choice!=-1 and 1<=choice<=5:
-    url+=("blacklistFlags=" + black[choice])
+    url+=("?blacklistFlags=" + black[choice])
 print("(Optional) Do you want to search joke that contains a string: ")
 print("Press Enter if don't want")
 string = input()
-url+=dic[n]
 if len(string)>0:
-    url+=("?contains=" + string)
+    if choice!=-1 and 1<=choice<=5:
+        url+='&'
+    else:
+        url+='?'
+    url+=("contains=" + string)
 r = requests.get(url)
 json_content = json.loads(r.content)
 print("JOKE: ")
-if json_content["type"]=="single":
-    print(json_content["joke"])
-elif json_content["type"]=="twopart":
-    print("setup: " + json_content["setup"])
-    print("delivery: " + json_content["delivery"])
+if json_content["error"]==False:
+    if json_content["type"]=="single":
+        print(json_content["joke"])
+    elif json_content["type"]=="twopart":
+        print("{:<10}".format("setup:") + json_content["setup"])
+        print("{:<10}".format("delivery:") + json_content["delivery"])
+else:
+    print("{:<16}".format("message:") + json_content["message"])
+    print("{:<16}".format("causedBy:") + json_content["causedBy"][0])
+    for i in range(1, len(json_content["causedBy"])):
+        print("{:<16}".format(" ") + json_content["causedBy"][i])
+    print("{:<16}".format("additionalInfo: ") + json_content["additionalInfo"])
